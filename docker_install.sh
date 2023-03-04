c34d81ef0381d1176f1182da5db0962b4c417e17
rnmrnmrn <124680649+rnmrnmrn@users.noreply.github.com>
2023-03-05 01:22:58 +0800

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#彩色
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+curl -fLsS https://get.docker.com/ | sh
