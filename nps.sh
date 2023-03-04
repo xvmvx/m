e8d7aeca58c017c0b03f9f806d0cce9b54cd8185
rnmrnmrn <124680649+rnmrnmrn@users.noreply.github.com>
2023-03-05 02:03:24 +0800

    Create nps.sh

---
+++ nps.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+
+
+logo="$(tput setaf 6)
+   ____                 ___        __
+  / ___| ___   ___   __| \ \      / /_ _ _   _ 
+ | |  _ / _ \ / _ \ / _` |\ \ /\ / / _` | | | |
+ | |_| | (_) | (_) | (_| | \ V  V / (_| | |_| |
+  \____|\___/ \___/ \__,_|  \_/\_/ \__,_|\__, |
+                                         |___/ 
+$(tput sgr0)"
+
+logo1="$(tput setaf 6)
+    ▄▄▄▄                             ▄▄ ▄▄      ▄▄                    
+  ██▀▀▀▀█                            ██ ██      ██                    
+ ██         ▄████▄    ▄████▄    ▄███▄██ ▀█▄ ██ ▄█▀  ▄█████▄  ▀██  ███ 
+ ██  ▄▄▄▄  ██▀  ▀██  ██▀  ▀██  ██▀  ▀██  ██ ██ ██   ▀ ▄▄▄██   ██▄ ██  
+ ██  ▀▀██  ██    ██  ██    ██  ██    ██  ███▀▀███  ▄██▀▀▀██    ████▀  
+  ██▄▄▄██  ▀██▄▄██▀  ▀██▄▄██▀  ▀██▄▄███  ███  ███  ██▄▄▄███     ███   
+    ▀▀▀▀     ▀▀▀▀      ▀▀▀▀      ▀▀▀ ▀▀  ▀▀▀  ▀▀▀   ▀▀▀▀ ▀▀     ██    
+                                                              ███     
+$(tput sgr0)"
+if [[ -f /etc/redhat-release ]]; then
+  release="centos"
+elif cat /etc/issue | grep -q -E -i "debian"; then
+  release="debian"
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+  release="ubuntu"
+elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+  release="centos"
+elif cat /proc/version | grep -q -E -i "debian"; then
+  release="debian"
+elif cat /proc/version | grep -q -E -i "ubuntu"; then
+  release="ubuntu"
+elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+  release="centos"
+fi
+wget -N --no-check-certificate  https://raw.githubusercontent.com/xvmvx/new/main/MY.sh && chmod +x MY.sh  && bash MY.sh
+myIP1=$(curl ip.sb)
+myIP2=$(ip route get 1 | awk '{print $7;exit}')
+clear
+yellow "${logo1}"
+red "########################################"
+blue   "国内主机请选择1，国外主机请选择2"
+read -e -p "(输入为空则取消):" inMY
+if [[ "$inMY" = "1" ]]; then
+  blue   "服务端请选择1，客户端请选择2"
+  read -e -p "(输入为空则取消):" insc
+  if [[ "$insc" = "1" ]]; then
+    wget http://niu.tel-com.cc/v/npc/l_s.tar.gz
+    tar zxvf l_s.tar.gz
+    chmod +x *
+    sudo ./nps install
+    /etc/nps
+    sudo nps start
+
+
+green "docker优化："
+yellow "docker开通IP6               （1）"
+yellow "docker更换国内源             （2）"
+green "docker面板安装："
+yellow "Fast OS Docker面板          （3）"
+yellow "Portainer面板               （4）"
+blue   "拭目以待结果，lets go"
